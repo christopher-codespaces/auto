dffbc22ba8682c30fe0d29c7fa09bbcb067801d3
SwiftlyBox <paymentsswiftlybox@gmail.com>
2023-09-28 19:34:40 +0200

    Shipped FUck

--- components/About.jsx
+++ components/About.jsx
@@ -1,40 +1,49 @@
-const CTA = () => {
-    return (
-      <section className="cta-sec relative max-w-screen-xl mx-auto py-4 px-4 md:px-8">
-        <div className="absolute top-0 left-0 w-full h-full bg-white opacity-40"></div>
-        <div className="relative z-10 gap-5 items-center lg:flex">
-          <div className="flex-1 max-w-lg py-5 sm:mx-auto sm:text-center lg:max-w-max lg:text-left">
-            <h3 className="text-3xl text-gray-800 font-semibold md:text-4xl">
-              {`Drive with Confidence              `}
-            </h3>
-            <p className="text-gray-500 leading-relaxed mt-3">
-            Experience the Power of Professional Auto Electrical and Diagnostics Services Today!
+import React from "react";
+import { FaCarBattery, FaBolt, FaWrench } from 'react-icons/fa';
+
+const About = () => {
+  return (
+    <section className="py-16 bg-gray-100">
+      <div className="max-w-screen-xl mx-auto text-gray-600 px-8">
+        <div className="text-center mb-12">
+          <h2 className="text-3xl font-bold text-indigo-600 mb-2">About Us</h2>
+          <p className="text-base text-gray-700">
+            Offering a range of services to keep your vehicle in optimal condition.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div className="p-6 bg-white rounded-lg shadow-md">
+            <FaBolt className="text-4xl text-indigo-600 mb-4" />
+            <h3 className="text-lg font-semibold mb-2">Computer Diagnostic Testing</h3>
+            <p className="text-sm text-gray-700">
+              Our advanced computer diagnostic testing ensures accurate identification of issues in your {`vehicle's`} systems.
             </p>
-            <a
-              className="mt-5 px-4 py-2 text-indigo-600 font-medium bg-indigo-50 rounded-full inline-flex items-center"
-              href="contact">
-              Contact Us
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-1 duration-150" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7l5 5m0 0l-5 5m5-5H6" />
-              </svg>
-            </a>
           </div>
-          <div className="flex-1 mt-5 mx-auto sm:w-9/12 lg:mt-0 lg:w-auto">
-            <img
-              src="https://xtremeautoelectrics.co.za/wp-content/uploads/elementor/thumbs/pexels-burak-the-weekender-86993-q54qokgvbnee7mn0v2sm8d3usksntx99s14tnr2e14.jpg"
-              alt="Auto Electrical Services"
-              className="w-full rounded-md"
-            />
+          <div className="p-6 bg-white rounded-lg shadow-md">
+            <FaWrench className="text-4xl text-indigo-600 mb-4" />
+            <h3 className="text-lg font-semibold mb-2">Transmission Service & Repair</h3>
+            <p className="text-sm text-gray-700">
+              Expert service and repair for your {`vehicle's`} transmission, ensuring smooth and reliable performance.
+            </p>
+          </div>
+          <div className="p-6 bg-white rounded-lg shadow-md">
+            <FaCarBattery className="text-4xl text-indigo-600 mb-4" />
+            <h3 className="text-lg font-semibold mb-2">Battery Replacements</h3>
+            <p className="text-sm text-gray-700">
+              Swift and efficient battery replacement services to keep your vehicle powered and ready to go.
+            </p>
+          </div>
+          <div className="p-6 bg-white rounded-lg shadow-md">
+            <FaBolt className="text-4xl text-indigo-600 mb-4" />
+            <h3 className="text-lg font-semibold mb-2">Emissions Diagnostics</h3>
+            <p className="text-sm text-gray-700">
+              Accurate diagnostics for emissions-related issues, ensuring compliance with environmental standards.
+            </p>
           </div>
         </div>
+      </div>
+    </section>
+  );
+};
 
-        <style jsx>{`
-          .cta-sec {
-            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23e5e7eb' fill-opacity='0.4'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm-10 0v-9h-9v9h9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
-          }
-        `}</style>
-      </section>
-    );
-  };
-
-  export default CTA;
+export default About;

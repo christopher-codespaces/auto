558cf5025f469ea1631e35c32afefb6eed765fdf
SwiftlyBox <paymentsswiftlybox@gmail.com>
2023-09-28 19:29:30 +0200

    Build SUccesss

--- components/index.js
+++ components/index.js
@@ -1,8 +1,9 @@
 export {default as NavBar} from "./NavBar.jsx";
-export {default as CTA} from "./CTA.jsx";
+export {default as CTA} from "./About.jsx";
 export {default as Hero} from "./Hero.jsx";
 export {default as Model} from "./Model.jsx";
 export {default as Contact} from "./Contact.jsx";
 export {default as Footer} from "./Footer.jsx"
 export {default as Pass} from "./Pass.jsx";
 export {default as Services} from "./Services.jsx"
+export {default as About} from "./About.jsx"
